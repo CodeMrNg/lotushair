c7dbcf7a426781d84fb1ba46887e1fb8a71d701d
divinmik <mikdivin@gmail.com>
2026-06-12 22:13:57 +0100

    fix: Add database timeout option to prevent "database is locked" errors

--- lotushair/settings.py
+++ lotushair/settings.py
@@ -79,6 +79,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'OPTIONS': {
+            'timeout': 60,  # Temps en secondes avant de renvoyer "database is locked"
+        },
     }
 }
 
